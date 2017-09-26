f71b1d7b757e131418985485b02d2bd75c9d2602
Marlon Monroy <monroy@Marlons-MacBook-Pro.local>
2017-09-25 22:51:11 -0700

    fixing an error

--- outhConfig.js
+++ outhConfig.js
@@ -1,11 +1,11 @@
 const ClientOAuth2 = require('client-oauth2');
-const  port = process.env.port || 3000;
+const port = process.env.PORT || 8080;
 const nprAuth = new ClientOAuth2({
     clientId:'nprone_trial_QpygBBVs2ywc',
     clientSecret:'zsQ9mXCPMEFTCdTbybAmHOEEXDhdEINGj3YoMEvr',
     accessTokenUri:'https://api.npr.org/authorization/v2/token',
     authorizationUri:'https://api.npr.org/authorization/v2/authorize',
-    redirectUri:'http://localhost:{port}/callback',
+    redirectUri:'https://npr-client.herokuapp.com/callback',
     scopes: ['identity.readonly','listening.readonly'],
     state:'nc98P987bcpncYhoadjoiydc9ajDlcn'
 });
