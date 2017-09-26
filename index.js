2dcc1320d818d4e2d3524994bc12b5588a8d2216
Marlon Monroy <monroy@Marlons-MacBook-Pro.local>
2017-09-25 22:46:32 -0700

    fixing an error

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const  port = process.env.port || 3000;
+const port = process.env.PORT || 8080;
 app.use(bodyParser.json());
 
 app.use(router);
